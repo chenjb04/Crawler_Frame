141f1e84809a31f1c6830d0f8fe410ead15be21c
chenjb04 <chenjb04@163.com>
2019-03-13 17:53:54 +0800

    scheduler对象封装雏形

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,3 +1,36 @@
 # -*- coding:utf-8 -*-
 __author__ = 'ChenJiaBao'
 __date__ = '2019/3/13 17:00'
+# 调度器封装
+from queue import Queue
+
+
+class Scheduler(object):
+    """
+    scheduler:缓存请求对象，为下载器提供请求对象，对请求对象进行去重
+    """
+    def __init__(self):
+        self.queue = Queue()
+
+    def add_request(self, request):
+        """
+        添加request到队列
+        :param request: 请求对象
+        :return:
+        """
+        self.queue.put(request)
+
+    def get_request(self):
+        """
+        从队列中取request对象
+        :return:
+        """
+        return self.queue.get()
+
+    def _filter_request(self, request):
+        """
+        对request进行去重
+        :param request:请求对象
+        :return:
+        """
+        pass
