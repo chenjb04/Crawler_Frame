9769876bd73d8daf547753ad2a8032afb92c6841
chenjb04 <chenjb04@163.com>
2019-03-13 17:37:24 +0800

    Item对象封装

--- item.py
+++ item.py
@@ -1,3 +1,24 @@
 # -*- coding:utf-8 -*-
 __author__ = 'ChenJiaBao'
 __date__ = '2019/3/13 17:02'
+# item对象
+
+
+class Item(object):
+    """
+    框架内置Item对象
+    """
+    def __init__(self, data):
+        """
+        初始化item对象
+        :param data: 传入的数据
+        """
+        self._data = data
+
+    @property
+    def data(self):
+        """
+        使data只读,保护data
+        :return:
+        """
+        return self._data
