338971dd7050594df5ee8e3bf27ce15aa63aaa8d
chenjb04 <chenjb04@163.com>
2019-03-13 20:36:23 +0800

    配置文件实现

---
+++ scrapy_plus/conf/settings.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+__author__ = 'ChenJiaBao'
+__date__ = '2019/3/13 20:22'
+from .default_settings import *
+# 这里导入的settings，是项目文件夹的settings文件
+from settings import *
--- scrapy_plus/utils/log.py
+++ scrapy_plus/utils/log.py
@@ -3,12 +3,7 @@ __author__ = 'ChenJiaBao'
 __date__ = '2019/3/13 20:02'
 import sys
 import logging
-
-# 默认的配置
-DEFAULT_LOG_LEVEL = logging.INFO    # 默认等级
-DEFAULT_LOG_FMT = '%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s: %(message)s'   # 默认日志格式
-DEFUALT_LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'  # 默认时间格式
-DEFAULT_LOG_FILENAME = 'log.log'    # 默认日志文件名称
+from scrapy_plus.conf.settings import DEFAULT_LOG_LEVEL, DEFAULT_LOG_FMT, DEFUALT_LOG_DATEFMT,DEFAULT_LOG_FILENAME
 
 
 class Logger(object):
