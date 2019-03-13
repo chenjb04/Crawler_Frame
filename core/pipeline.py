51fc0d11dffb2838b400d50a9ad6dd08ddd4f4e0
chenjb04 <chenjb04@163.com>
2019-03-13 18:05:39 +0800

    pipeline对象封装雏形

--- core/pipeline.py
+++ core/pipeline.py
@@ -1,3 +1,18 @@
 # -*- coding:utf-8 -*-
 __author__ = 'ChenJiaBao'
 __date__ = '2019/3/13 17:01'
+# 管道
+
+
+class Pipeline(object):
+    """
+    管道对象封装
+    """
+
+    def process_item(self, item):
+        """
+        处理item
+        :param item: item对象
+        :return:
+        """
+        print("item:", item)
