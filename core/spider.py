6fdb27ecdc589ffa1b36f037b38d43249b7c039a
chenjb04 <chenjb04@163.com>
2019-03-13 17:45:58 +0800

    spider对象封装雏形

--- core/spider.py
+++ core/spider.py
@@ -1,3 +1,30 @@
 # -*- coding:utf-8 -*-
 __author__ = 'ChenJiaBao'
 __date__ = '2019/3/13 17:01'
+# spider组件
+from http.request import Request
+from item import Item
+
+
+class Spider(object):
+    """
+    spider对象封装
+    """
+    start_url = 'http://www.baidu.com'
+
+    def start_requests(self):
+        """
+        构造start_url地址的请求
+        :return:
+        """
+        return Request(self.start_url)
+
+    def parse(self, response):
+        """
+        处理start_url请求的响应
+        :param response: response对象
+        :return:
+        """
+        return Item(response.body)
+
+
