633a7871033686c064d82afc8b2e19ae1f371aa4
chenjb04 <chenjb04@163.com>
2019-03-14 13:43:42 +0800

    实现多个爬虫

--- scrapy_plus/core/engine.py
+++ scrapy_plus/core/engine.py
@@ -18,14 +18,14 @@ class Engine(object):
     """
     提供程序入口，调用其他组件，实现整个框架的运作
     """
-    def __init__(self, spider):
+    def __init__(self, spiders):
         """
         初始化各个组件
         """
         self.scheduler = Scheduler()
         self.downloader = Downloader()
         self.pipeline = Pipeline()
-        self.spider = spider
+        self.spiders = spiders
         self.spider_middleware = SpiderMiddleware()
         self.downloader_middleware = DownloaderMiddleware()
         self.total_request_num = 0
@@ -50,13 +50,16 @@ class Engine(object):
         初始化请求，调用spider的start_request方法，所有请求添加到调度器
         :return:
         """
-        # 调用spider start_request方法，获取request对象
-        for start_request in self.spider.start_requests():
-            # 对start_request处理
-            start_request = self.spider_middleware.process_request(start_request)
-            # 调用scheduler add_request方法，添加到调度器
-            self.scheduler.add_request(start_request)
-            self.total_request_num += 1
+        for spider_name, spider in self.spiders.items():
+            # 调用spider start_request方法，获取request对象
+            for start_request in spider.start_requests():
+                # 对start_request处理
+                start_request = self.spider_middleware.process_request(start_request)
+                # 初始请求添加spider_name
+                start_request.spider_name = spider_name
+                # 调用scheduler add_request方法，添加到调度器
+                self.scheduler.add_request(start_request)
+                self.total_request_num += 1
 
     def _execute_request_response_item(self):
         """
@@ -77,12 +80,14 @@ class Engine(object):
         response = self.downloader_middleware.process_response(response)
         response = self.spider_middleware.process_response(response)
         # 获取request对象响应的parse方法
-        parse = getattr(self.spider, request.parse)
+        spider = self.spiders[request.spider_name]
+        parse = getattr(spider, request.parse)
         # 调用爬虫的parse方法， 处理响应
         for result in parse(response):
             # 判断结果，如果为request对象，重新调用调度器的add_request方法
             if isinstance(result, Request):
                 result = self.spider_middleware.process_request(result)
+                result.spider_name = request.spider_name
                 self.scheduler.add_request(result)
                 self.total_request_num += 1
             # 如果不是，调用pipeline的process_item方法,处理结果
